52a9969626418bc3cc7bb509eceba703083af08e
lhexic <azanujonathan6996@gmail.com>
2026-08-13 12:01:41 +0000

    new day

--- week06/EternalQuest/obj/Debug/net10.0/EternalQuest.AssemblyInfo.cs
+++ week06/EternalQuest/obj/Debug/net10.0/EternalQuest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EternalQuest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+286a2488dae12c132150639812b2ea2b86365497")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+905406102fafed2cd7449e0a3aa62955aefe3c8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EternalQuest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EternalQuest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
